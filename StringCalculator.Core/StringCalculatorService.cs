974acc76caa946bcb7eba65348a7c1aa20131322
Michael <michael.a.heimsch@gmail.com>
2022-04-17 11:18:24 +0200

    State after 30 minutes

---
+++ StringCalculator.Core/StringCalculatorService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Core
+{
+    public class StringCalculatorService
+    {
+        public int Add(string numbers)
+        {
+            int result = 0;
+
+            if (numbers == null)
+            {
+                return result;
+            }
+
+            numbers = ReplaceNewLineWithComma(numbers);
+
+            foreach (var part in PartsOfCSV(numbers))
+            {
+                result += int.Parse(part);
+            }
+            
+            return result;
+        }
+
+        private string ReplaceNewLineWithComma(string numbers)
+        {
+            return numbers.Replace("\n", ",");
+        }
+
+        private string[] PartsOfCSV(string numbers)
+        {
+            string[] result;
+            return numbers.Split(',');
+        }
+
+    }
+}
---
+++ StringCalculator.Tests/StringCalculatorServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Core.Tests
+{
+    [TestClass()]
+    public class StringCalculatorServiceTests
+    {
+        private StringCalculatorService _sut = new StringCalculatorService();
+        [TestMethod()]
+        public void Add_ShouldReturnZeroIfStringIsNull()
+        {
+            int expected = 0;
+            int actual = _sut.Add(null);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Add_ShouldReturnNumberOfStringInput()
+        {
+            int expected = 2;
+            int actual = _sut.Add("2");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Add_ShouldReturnSumOfStringInputs()
+        {
+            int expected = 3;
+            int actual = _sut.Add("1,2");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Add_ShouldReturnSumOfUnknownNumberOfStringInputs()
+        {
+            int expected = 13;
+            int actual = _sut.Add("1,2,7,3");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Add_ShouldAllowNewLineForStringInputs()
+        {
+            int expected = 6;
+            int actual = _sut.Add("1\n2,3");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Add_ShouldAllowChangingDelimitersStringInputs()
+        {
+            int expected = 3;
+            int actual = _sut.Add("//;\n1;2");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
